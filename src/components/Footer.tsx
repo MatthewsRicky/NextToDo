db8ec3f9210872c77a0c1f2fd2ea0a66ac36ae5f
RickyMatthews <matthewsrickypro@gmail.com>
2024-07-11 18:44:26 +0300

    building out the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="flex justify-between w-[972px] items-center text-[11px] mt-[17px]">
+      <small className="text-[11px]">&copy; 2024 copyright by Ricky</small>
+      <p>
+        version <b>1.5</b>
+      </p>
+    </footer>
+  );
+}
