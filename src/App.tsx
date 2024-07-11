bcc65b78cc2f90c8d710cd44447ce7a35b69858b
RickyMatthews <matthewsrickypro@gmail.com>
2024-07-11 11:52:18 +0300

    building out the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 function App() {
   return (
-    <div className="font-sans bg-[#F1D4B3] min-h-screen">
-      <h1 className="text-[120px] font-bold text-black/5 absolute left-1/2 -translate-x-1/2">
+    <div className="flex justify-center items-center font-sans bg-[#F1D4B3] min-h-screen">
+      <h1 className="text-[120px] font-bold text-black/5 absolute left-1/2 -translate-x-1/2 tracking-[0.2rem]">
         To Do App
       </h1>
+
+      <main className="relative w-[972px] rounded-[8px] bg-white h-[636px] shadow-[0_4px_4px_rgba(0,0,0,0.8)]">
+        <header></header>
+        <ul></ul>
+        <section></section>
+      </main>
+      <footer></footer>
     </div>
   );
 }
