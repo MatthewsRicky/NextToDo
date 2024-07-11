7e765a1c3b11267a1fe994648f1da08ae2878baf
RickyMatthews <matthewsrickypro@gmail.com>
2024-07-11 12:34:42 +0300

    bg heading component creation

---
+++ src/components/BackgroundHeading.tsx
@@ -0,0 +1,9 @@
+export default function BackgroundHeading() {
+  return (
+    <div>
+      <h1 className="text-[120px] font-bold text-black/5 absolute left-1/2 -translate-x-1/2 tracking-[0.2rem]">
+        To Do App
+      </h1>
+    </div>
+  );
+}
