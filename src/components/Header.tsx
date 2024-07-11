39e5976fe3b48c62fa32cd127aee34cd7b45188f
RickyMatthews <matthewsrickypro@gmail.com>
2024-07-11 18:55:16 +0300

    icon image link from bytegrad (subject to change if I find others)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
     <div>
-      <header className="col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]"></header>
+      <header className="col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
+        <img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" />
+      </header>
     </div>
   );
 }
