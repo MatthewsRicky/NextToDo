a1204e8a693668b99db961146a411570e5f302fc
RickyMatthews <matthewsrickypro@gmail.com>
2024-07-11 12:34:19 +0300

    factorizing and import of background heading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,8 @@
+import BackgroundHeading from "./BackgroundHeading";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center font-sans bg-[#F1D4B3] min-h-screen">
-      <h1 className="text-[120px] font-bold text-black/5 absolute left-1/2 -translate-x-1/2 tracking-[0.2rem]">
-        To Do App
-      </h1>
+      <BackgroundHeading />
 
       <main className="relative w-[972px] rounded-[8px] bg-white h-[636px] shadow-[0_4px_4px_rgba(0,0,0,0.8)] grid grid-cols-[7fr_4fr] grid-rows-[50px_1fr] overflow-hidden">
         <header className="col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]"></header>
