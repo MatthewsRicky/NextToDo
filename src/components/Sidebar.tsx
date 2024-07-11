0c15079fda2668fbc79e6df2b496de65679cbd2c
RickyMatthews <matthewsrickypro@gmail.com>
2024-07-11 18:45:27 +0300

    sidebar scafold

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+  return (
+    <div>
+      {" "}
+      <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]"></section>
+    </div>
+  );
+}
