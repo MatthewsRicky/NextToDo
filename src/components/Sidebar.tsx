c3d9ac9fa83cfa198d9f5e0fc9108a40cc3ef50b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-12 08:29:55 +0000

    sidebar layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ export default function Sidebar() {
   return (
     <div>
       {" "}
-      <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]">
+      <section className="flex col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]">
         <form>
           <h1>Add a new To do</h1>
           <input type="text" placeholder="Add a new task..." />
