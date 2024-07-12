5e1ecc607ba3fc7b0209a7e93be4d26dd8ddbf1a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-12 08:21:37 +0000

    Form scafold

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,13 @@ export default function Sidebar() {
   return (
     <div>
       {" "}
-      <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]"></section>
+      <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]">
+        <form>
+          <h1>Add a new To do</h1>
+          <input type="text" placeholder="Add a new task..." />
+          <button type="submit">Add</button>
+        </form>
+      </section>
     </div>
   );
 }
