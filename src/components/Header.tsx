a4f814825356da8048ae12da29e0414ed1206287
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-13 09:43:19 +0000

    header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 export default function Header() {
   return (
     <div>
-      <header className="col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
+      <header className="flex justify-between items-center px-2 col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
         <img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" />
+
+        <p>
+          <b>0</b> / 0 todos completed
+        </p>
       </header>
     </div>
   );
