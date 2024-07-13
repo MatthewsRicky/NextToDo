922bfb8548de9d91ba319007823afe005f7f70ec
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-13 09:36:06 +0000

    layout troubleshoot

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="flex flex-col justify-center items-center font-sans bg-[#F1D4B3] min-h-screen">
       <BackgroundHeading />
 
-      <main className="relative w-[972px] rounded-[8px] bg-white h-[636px] shadow-[0_4px_4px_rgba(0,0,0,0.8)] grid grid-cols-[7fr_4fr] grid-rows-[50px_1fr] overflow-hidden">
+      <main className="relative flex w-[972px] rounded-[8px] bg-white h-[636px] shadow-[0_4px_4px_rgba(0,0,0,0.8)] grid-cols-[7fr_4fr] grid-rows-[50px_1fr] overflow-hidden">
         <Header />
         <TodoList />
         <Sidebar />
