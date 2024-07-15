c7050a9183d6d9ce37d8ba35a13167747b75839f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-15 08:22:35 +0000

    final border

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,10 +4,7 @@ export default function Sidebar() {
       <section className="flex col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]">
         <form>
           <h1>Add a new To do</h1>
-          <input
-            type="text"
-            className="px-2 py-3 border-solid border-black/70"
-          />
+          <input type="text" className="px-2 py-3 border border-black/30" />
           <button type="submit">Add</button>
         </form>
       </section>
