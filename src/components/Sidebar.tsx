1e6cf4e86298f2739f27d79e162d5f87615d7cf0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-15 08:11:03 +0000

    removing the placeholder text in the input field

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
       <section className="flex col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[8%]">
         <form>
           <h1>Add a new To do</h1>
-          <input type="text" placeholder="Add a new task..." />
+          <input type="text" />
           <button type="submit">Add</button>
         </form>
       </section>
