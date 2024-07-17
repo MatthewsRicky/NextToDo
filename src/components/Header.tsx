1a1256574716a562801c3948b7b9563f73bffc0b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-17 17:54:51 +0000

    trying to fix the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div>
-      <header className="flex justify-between items-center px-2 col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
+      <header className="flex justify-between items-center px-2 col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%] p-4">
         <img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" />
 
         <p>
