6738f66ee76a42fd240720110858070c9cbe5745
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-18 17:54:10 +0000

    layout styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div>
-      <header className="flex justify-between items-center px-2 col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%] w-full p-4">
+      <header className="flex justify-between items-center px-28px col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
         <img src="https://bytegrad.com/course-assets/react-nextjs/dots.png" />
 
         <p>
