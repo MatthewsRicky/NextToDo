5ebfbf690ce8100ad11eadbf3b8c75fcaabc1225
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-18 17:46:59 +0000

    styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,10 +5,10 @@ import Sidebar from "./Sidebar";
 import TodoList from "./TodoList";
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center font-sans bg-[#F1D4B3] min-h-screen">
+    <div className="flex justify-center items-center font-sans bg-[#F1D4B3] min-h-screen">
       <BackgroundHeading />
 
-      <main className="relative grid w-[972px] rounded-[8px] bg-white h-[636px] shadow-[0_4px_4px_rgba(0,0,0,0.8)] grid-cols-[7fr_4fr] grid-rows-[50px_1fr] overflow-hidden">
+      <main className="relative w-[972px] rounded-[8px] bg-white h-[636px] shadow-[0_4px_4px_rgba(0,0,0,0.8)] grid grid-cols-[7fr_4fr] grid-rows-[50px_1fr] overflow-hidden">
         <Header />
         <TodoList />
         <Sidebar />
