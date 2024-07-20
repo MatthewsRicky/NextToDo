2be0823a17b098e31b708cf5fae00fb3f8d7ddc1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-20 18:06:44 +0000

    refactoring the counter component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,6 @@ export default function Header() {
     <div>
       <header className="flex justify-between items-center px-28px col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
         <Logo />
-        <p>
-          <b>0</b> / 0 todos completed
-        </p>
       </header>
     </div>
   );
