b3bf3e06fe2be209dd8c32e2bbfe63650443c4e9
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-20 18:04:55 +0000

    importing the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import Logo from "./Logo";
+
 export default function Header() {
   return (
     <div>
       <header className="flex justify-between items-center px-28px col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
+        <Logo />
         <p>
           <b>0</b> / 0 todos completed
         </p>
