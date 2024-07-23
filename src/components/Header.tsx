bf68f3d37bfb437319aaa57c8442c74f6bb3e907
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-23 10:34:24 +0000

    error corerctions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 export default function Header() {
   return (
     <div>
-      <header className="flex justify-between items-center px-2 col-[1/3] row-[1/2 bg-[#fbf5ed] border-b border-black/[8%]">
+      <header className="flex justify-between items-center px-2 col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[8%]">
         <Logo />
         <Counter />
       </header>
