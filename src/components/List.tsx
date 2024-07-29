67fcd99a9b42a326c52a0f4c1c6ac51983b8abaa
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-07-29 12:53:25 +0000

    initializing the list component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,11 @@
 export default function List() {
-  return <div>List</div>;
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
