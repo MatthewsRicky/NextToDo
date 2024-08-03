2532d81a776d997cefb92a176c5772b00425c475
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-08-03 17:22:29 +0000

    fleshing out the form component

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,3 +1,11 @@
 export default function AddTodoForm() {
-  return <div>Add</div>;
+  return (
+    <div>
+      <h2>Add To Do</h2>
+      <form>
+        <input type="text" />
+        <button type="submit">Add To List</button>
+      </form>
+    </div>
+  );
 }
