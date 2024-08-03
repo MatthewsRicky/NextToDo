e433e29efdce6794095e46e1d6956b2b914dda3a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-08-03 17:07:02 +0000

    centering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Logo from "./Logo";
 export default function Header() {
   return (
     <div>
-      <header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[8%] py-4">
+      <header className="flex justify-between items-center px-[28px] col-[1/3] row-[1/2] bg-[#fbf5ed] border-b border-black/[8%] py-4 mx-auto">
         <Logo />
         <Counter />
       </header>
