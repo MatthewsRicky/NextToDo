3599038840c45d8602e1bef7ce1a208ba82cbb0c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-08-04 20:05:11 +0000

    padding

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,7 +1,9 @@
 export default function AddTodoForm() {
   return (
     <div>
-      <h2 className="font-medium bg-[#231d15] text-white">Add To Do</h2>
+      <h2 className="font-medium bg-[#231d15] px-2 py-4 text-white">
+        Add To Do
+      </h2>
       <form>
         <input type="text" />
         <button type="submit">Add To List</button>
