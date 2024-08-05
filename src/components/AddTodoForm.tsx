f73f8b20409381829aad04eb84eb224f08f67021
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-08-05 19:13:06 +0000

    playing with backgrounds

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,12 +1,12 @@
 export default function AddTodoForm() {
   return (
     <div className="flex flex-col">
-      <h2 className="text-center font-medium bg-[#231d15] px-2 py-4 text-white">
+      <h2 className="text-center font-medium bg-[#231d15]/70 px-2 py-4 text-white">
         Add To Do
       </h2>
       <form>
         <input type="text" />
-        <button className="rounded-md px-2 py-2" type="submit">
+        <button className="bg-[#231d15]/30 rounded-md px-2 py-2" type="submit">
           Add To List
         </button>
       </form>
