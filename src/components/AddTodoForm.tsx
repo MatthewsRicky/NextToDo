bb9f2276f57c1b352d68e04a4cbfe9949f2a7beb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-08-05 19:14:14 +0000

    input styling

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -5,7 +5,7 @@ export default function AddTodoForm() {
         Add To Do
       </h2>
       <form>
-        <input type="text" />
+        <input className="border rounded-sm my-6 w-full" type="text" />
         <button className="bg-[#231d15]/30 rounded-md px-2 py-2" type="submit">
           Add To List
         </button>
