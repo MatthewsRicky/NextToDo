ecf2154da5a581652bac3e899dab3c755aea7c72
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2024-08-14 20:41:16 +0000

    flex on the p tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex justify-between w-[972px] items-center p-4 text-[11px] mt-[17px]">
       <small className="text-[11px]">&copy; 2024 copyright by Ricky</small>
-      <p>
+      <p className="flex">
         version <b>1.5</b>
       </p>
     </footer>
